4bea02bf0c0621b56a729598451910e42c656ec1
Miguel Espinoza <espinozamiguel349@gmail.com>
2018-04-03 16:08:13 -0400

    Cold Startup Improvements
    WIP Mattermost Start Component for lazy loading modules
    
    Add files changed for native modules
    
    Add Entry component and app global object
    
    dispatch setStatusBarHeight for iOS
    
    Update screen imports
    
    Include Entry screen
    
    Refactor app to mattermost.android.js
    
    Override unnecessary java files
    
    Fix minor issues in changes
    
    Display empty state based on user credentials
    
    Also, add proper background theme for empty loading screen
    
    Add native module constant cache support
    
    Fix startup theme regression
    
    Fix Orientation regression
    
     Fix SharedExtension regression
    
    Emit NATIVE_APP_LAUNCHED across bridge only once during cold start
    
    Add iOS Support
    
    Add Keychain support for credentials
    
    Revert to previous implementation of i18n
    
    Fix styling issues
    
    Stripped localization files
    
    Add RN unbundle support
    
    Upgrade JSCore Engine
    
    Optimize unbundle
    
    Capture launchTime with Telemetry class
    
    Include listener for SERVER_VERSION_CHANGED
    
    Avoid native bridge in cold start
    
    Temporary disable support for iOS
    
    Add DEFAULT_LOCALE after bad rebasing
    
    Remove toastLaunchTime

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 // See License.txt for license information.
 
 /* eslint-disable no-unused-vars */
+import 'react-native/Libraries/Core/InitializeCore';
 import {AppRegistry, Platform} from 'react-native';
 
+import telemetry from 'app/utils/telemetry';
 import Mattermost from 'app/mattermost';
 import ShareExtension from 'share_extension/android';
 
@@ -11,4 +13,28 @@ if (Platform.OS === 'android') {
     AppRegistry.registerComponent('MattermostShare', () => ShareExtension);
 }
 
-const app = new Mattermost();
+if (__DEV__) {
+    const modules = require.getModules();
+    const moduleIds = Object.keys(modules);
+    const loadedModuleNames = moduleIds
+        .filter(moduleId => modules[moduleId].isInitialized)
+        .map(moduleId => modules[moduleId].verboseName);
+    const waitingModuleNames = moduleIds
+        .filter(moduleId => !modules[moduleId].isInitialized)
+        .map(moduleId => modules[moduleId].verboseName);
+
+    // make sure that the modules you expect to be waiting are actually waiting
+    console.log(
+        'loaded:',
+        loadedModuleNames,
+        'waiting:',
+        waitingModuleNames
+    );
+
+    //  grab this text blob, and put it in a file named packager/moduleNames.js
+    console.log(`module.exports = ${JSON.stringify(loadedModuleNames.sort())};`);
+}
+
+if (Platform.OS === 'ios') {
+    new Mattermost();
+}
