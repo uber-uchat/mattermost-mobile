afeb1b20762a1165ac96bd853dd7336063bc976d
Dan Seawel <dseawel@hotmail.com>
2019-04-12 15:01:59 -0500

    Syntax fix

--- android/app/src/main/java/com/mattermost/rnbeta/MainApplication.java
+++ android/app/src/main/java/com/mattermost/rnbeta/MainApplication.java
@@ -48,91 +48,92 @@ import com.wix.reactnativenotifications.core.JsIOHelper;
 import android.util.Log;
 
 public class MainApplication extends NavigationApplication implements INotificationsApplication, INotificationsDrawerApplication {
-  public NotificationsLifecycleFacade notificationsLifecycleFacade;
-  public Boolean sharedExtensionIsOpened = false;
-  public Boolean replyFromPushNotification = false;
+    public NotificationsLifecycleFacade notificationsLifecycleFacade;
+    public Boolean sharedExtensionIsOpened = false;
+    public Boolean replyFromPushNotification = false;
 
-  @Override
-  public boolean isDebug() {
-    return BuildConfig.DEBUG;
-  }
+    @Override
+    public boolean isDebug() {
+        return BuildConfig.DEBUG;
+    }
 
-  @NonNull
-  @Override
-  public List<ReactPackage> createAdditionalReactPackages() {
-    // Add the packages you require here.
-    // No need to add RnnPackage and MainReactPackage
-    return Arrays.<ReactPackage>asList(
-            new ImagePickerPackage(),
-            new RNBottomSheetPackage(),
-            new RNDeviceInfo(),
-            new CookieManagerPackage(),
-            new VectorIconsPackage(),
-            new SvgPackage(),
-            new LinearGradientPackage(),
-            new RNNotificationsPackage(this),
-            new LocalAuthPackage(),
-            new JailMonkeyPackage(),
-            new RNFetchBlobPackage(),
-            new MattermostPackage(this),
-            new RNSentryPackage(),
-            new ReactNativeExceptionHandlerPackage(),
-            new ReactNativeYouTube(),
-            new ReactVideoPackage(),
-            new RNReactNativeDocViewerPackage(),
-            new ReactNativeDocumentPicker(),
-            new SharePackage(this),
-            new KeychainPackage(),
-            new InitializationPackage(this),
-            new RNRecyclerviewListPackage(),
-            new RNCWebViewPackage(),
-            new RNGestureHandlerPackage()
-    );
-  }
+    @NonNull
+    @Override
+    public List<ReactPackage> createAdditionalReactPackages() {
+        // Add the packages you require here.
+        // No need to add RnnPackage and MainReactPackage
+        return Arrays.<ReactPackage>asList(
+                new ImagePickerPackage(),
+                new RNBottomSheetPackage(),
+                new RNDeviceInfo(),
+                new CookieManagerPackage(),
+                new VectorIconsPackage(),
+                new SvgPackage(),
+                new LinearGradientPackage(),
+                new RNNotificationsPackage(this),
+                new LocalAuthPackage(),
+                new JailMonkeyPackage(),
+                new RNFetchBlobPackage(),
+                new MattermostPackage(this),
+                new RNSentryPackage(),
+                new ReactNativeExceptionHandlerPackage(),
+                new ReactNativeYouTube(),
+                new ReactVideoPackage(),
+                new RNReactNativeDocViewerPackage(),
+                new ReactNativeDocumentPicker(),
+                new SharePackage(this),
+                new KeychainPackage(),
+                new InitializationPackage(this),
+                new RNRecyclerviewListPackage(),
+                new RNCWebViewPackage(),
+                new RNGestureHandlerPackage()
+        );
+    }
 
-  @Override
-  public String getJSMainModuleName() {
-    return "index";
-  }
+    @Override
+    public String getJSMainModuleName() {
+        return "index";
+    }
 
-  @Override
-  public void onCreate() {
-    Log.i("MainApplication", "onCreate begin");
-    super.onCreate();
-    instance = this;
+    @Override
+    public void onCreate() {
+        Log.i("MainApplication", "onCreate begin");
+        super.onCreate();
+        instance = this;
 
-    // Delete any previous temp files created by the app
-    File tempFolder = new File(getApplicationContext().getCacheDir(), "mmShare");
-    RealPathUtil.deleteTempFiles(tempFolder);
-    Log.i("ReactNative", "Cleaning temp cache " + tempFolder.getAbsolutePath());
+        // Delete any previous temp files created by the app
+        File tempFolder = new File(getApplicationContext().getCacheDir(), "mmShare");
+        RealPathUtil.deleteTempFiles(tempFolder);
+        Log.i("ReactNative", "Cleaning temp cache " + tempFolder.getAbsolutePath());
 
-    // Create an object of the custom facade impl
-    notificationsLifecycleFacade = NotificationsLifecycleFacade.getInstance();
-    // Attach it to react-native-navigation
-    setActivityCallbacks(notificationsLifecycleFacade);
+        // Create an object of the custom facade impl
+        notificationsLifecycleFacade = NotificationsLifecycleFacade.getInstance();
+        // Attach it to react-native-navigation
+        setActivityCallbacks(notificationsLifecycleFacade);
 
-    SoLoader.init(this, /* native exopackage */ false);
-  }
+        SoLoader.init(this, /* native exopackage */ false);
+    }
 
-  @Override
-  public boolean clearHostOnActivityDestroy(Activity activity) {
-    // This solves the issue where the splash screen does not go away
-    // after the app is killed by the OS cause of memory or a long time in the background
-    return false;
-  }
+    @Override
+    public boolean clearHostOnActivityDestroy(Activity activity) {
+        // This solves the issue where the splash screen does not go away
+        // after the app is killed by the OS cause of memory or a long time in the background
+        return false;
+    }
 
-  @Override
-  public IPushNotification getPushNotification(Context context, Bundle bundle, AppLifecycleFacade defaultFacade, AppLaunchHelper defaultAppLaunchHelper) {
-    return new CustomPushNotification(
-            context,
-            bundle,
-            notificationsLifecycleFacade, // Instead of defaultFacade!!!
-            defaultAppLaunchHelper,
-            new JsIOHelper()
-    );
-  }
+    @Override
+    public IPushNotification getPushNotification(Context context, Bundle bundle, AppLifecycleFacade defaultFacade, AppLaunchHelper defaultAppLaunchHelper) {
+        return new CustomPushNotification(
+                context,
+                bundle,
+                notificationsLifecycleFacade, // Instead of defaultFacade!!!
+                defaultAppLaunchHelper,
+                new JsIOHelper()
+        );
+    }
 
-  @Override
-  public IPushNotificationsDrawer getPushNotificationsDrawer(Context context, AppLaunchHelper defaultAppLaunchHelper) {
-    return new CustomPushNotificationDrawer(context, defaultAppLaunchHelper);
-  }
+    @Override
+    public IPushNotificationsDrawer getPushNotificationsDrawer(Context context, AppLaunchHelper defaultAppLaunchHelper) {
+        return new CustomPushNotificationDrawer(context, defaultAppLaunchHelper);
+    }
+}
